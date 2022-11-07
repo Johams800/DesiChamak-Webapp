c6379cc3ecccd60e0797f1551bf736e1ae0a5081
Johams800 <jhammo@gmu.edu>
2022-11-07 13:52:10 -0500

    second security update

--- jewelryshop/settings.py
+++ jewelryshop/settings.py
@@ -12,8 +12,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0'
-
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '3%y3laftm62q0zaj+s7#p-xqq9(&#q+)s8)p-&#&bz*0$!xu$0')
 
 # SECURITY WARNING: don't run with debug turned on in production!
